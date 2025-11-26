b6726f2e6b4206e17ea64fa95b604311eae5cbb9
Xavier Chen <xaviernehc@gmail.com>
2025-11-27 02:05:30 +0800

    feat: add notebook for cleaning data

---
+++ 01_clean_data.py
@@ -0,0 +1,575 @@
+import marimo
+
+__generated_with = "0.18.1"
+app = marimo.App()
+
+with app.setup:
+    import marimo as mo
+    import pandas as pd
+    import numpy as np
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    START_DATE = "2025-07-01"
+    END_DATE = "2025-08-31"
+
+
+@app.cell(hide_code=True)
+def data_sources():
+    mo.md(r"""
+    # Data sources
+
+    ## Query Data Range
+    29 Aug 2020 - 29 Aug 2025
+
+    ## Options
+    Options data is queried from OptionMetrics through WRDS [link](https://wrds-www.wharton.upenn.edu/pages/get-data/optionmetrics/ivy-db-us/options/option-prices/)
+
+    ## Forward Prices
+    Forward Prices data is queried from OptionMetrics through WRDS [link](https://wrds-www.wharton.upenn.edu/pages/get-data/optionmetrics/ivy-db-us/options/forward-price/)
+
+    ## Interest Rates
+    Interest Rates data is queried from OptionMetrics through WRDS [link](https://wrds-www.wharton.upenn.edu/pages/get-data/optionmetrics/ivy-db-us/market/zero-coupon-yield-curve/)
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def read_md():
+    mo.md(r"""
+    # Read datasets
+    """)
+    return
+
+
+@app.cell
+def read_options():
+    options = pd.read_csv("options.csv")
+    options
+    return (options,)
+
+
+@app.cell
+def read_forwards():
+    forwards = pd.read_csv("forwards.csv")
+    forwards
+    return (forwards,)
+
+
+@app.cell
+def read_interests():
+    interests = pd.read_csv("interests.csv")
+    interests
+    return
+
+
+@app.cell(hide_code=True)
+def clean_options_md():
+    mo.md(r"""
+    ## Clean Options
+
+    Steps taken:
+    1. Keep necessary columns
+    2. Set the right dtypes
+    3. Filter for specific dates
+    4. Remove rows with null IVs due to special settlement type
+    5. Divide strike by 1000; In OptionMetric, strike is x 1000
+    6. (paper) Filter out no open interest and no volume
+    7. Filter out best_bid == 0 as they are not representative
+    8. Calculate mid option price
+    9. (paper) Filter mid_price < 1/8
+    10. Calculate days to expiry
+    11. (paper) Filter days_to_expiry > 120
+
+    Notes:
+    1. They are all European options
+    """)
+    return
+
+
+@app.cell
+def clean_options(options):
+    # 1. Keep necessary columns
+    options_tmp = options.loc[
+        :,
+        [
+            "date",
+            "cp_flag",
+            "strike_price",
+            "exdate",
+            "impl_volatility",
+            "best_bid",
+            "best_offer",
+            "am_settlement",
+            "open_interest",
+            "volume",
+        ],
+    ].copy()
+
+    print("Option's columns: ", "\n", options_tmp.columns, "\n")
+
+    # 2. Set the right dtypes
+    options_tmp["date"] = pd.to_datetime(options_tmp["date"])
+    options_tmp["exdate"] = pd.to_datetime(options_tmp["exdate"])
+
+    options_tmp = options_tmp.astype(
+        {
+            "cp_flag": "category",
+            "strike_price": "Int64",
+            "impl_volatility": "Float64",
+            "best_bid": "Float64",
+            "best_offer": "Float64",
+            "open_interest": "Int64",
+            "volume": "Int64",
+        }
+    )
+
+    print("Option's datatype check: ", "\n", options_tmp.dtypes, "\n")
+
+    # 3. Filter for specific dates
+    options_date_mask = (options_tmp["date"] >= START_DATE) & (
+        options_tmp["date"] <= END_DATE
+    )
+    options_tmp = options_tmp[options_date_mask].copy()
+
+    print("Maximum date: ", options_tmp["date"].max(), "\n")
+    print("Minimum date: ", options_tmp["date"].min(), "\n")
+
+    # 4. Remove rows with null IVs due to special settlement type
+    print("DF size before dropping null IV: ", len(options_tmp), "\n")
+
+    options_tmp = options_tmp.dropna(axis=0, subset=["impl_volatility"])
+
+    print("DF size after dropping null IV: ", len(options_tmp), "\n")
+
+    # 5. Divide strike by 1000; In OptionMetric, strike is x 1000
+    options_tmp["strike_price"] = options_tmp["strike_price"] / 1000
+
+    # 6. (paper) filter out no open interest and no volume
+    print(
+        "DF size before filtering out no open interest and no volume: ",
+        len(options_tmp),
+        "\n",
+    )
+
+    no_open_interest_volume_mask = (options_tmp["open_interest"] == 0) | (
+        options_tmp["volume"] == 0
+    )
+
+    print(
+        "DF size for options with no open interest and no open volume: ",
+        len(options_tmp[no_open_interest_volume_mask]),
+        "\n",
+    )
+
+    options_tmp = (
+        options_tmp.loc[~no_open_interest_volume_mask]
+        .drop(columns=["open_interest", "volume"])
+        .copy()
+    )
+
+    print(
+        "DF size after filtering out no open interest and no volume: ",
+        len(options_tmp),
+        "\n",
+    )
+
+    # 7. filter out best_bid == 0 as they are not representative
+    print(
+        "DF size before filtering out best_bid == 0: ",
+        len(options_tmp),
+        "\n",
+    )
+
+    best_bid_0_mask = options_tmp["best_bid"] == 0
+
+    print(
+        "DF size for options with best_bid == 0: ",
+        len(options_tmp[best_bid_0_mask]),
+        "\n",
+    )
+
+    options_tmp = options_tmp.loc[~best_bid_0_mask].copy()
+
+    print(
+        "DF size after filtering out best_bid == 0: ",
+        len(options_tmp),
+        "\n",
+    )
+
+    # 8. calculate mid option price
+    options_tmp["mid_price"] = (options_tmp["best_bid"] + options_tmp["best_offer"]) / 2
+    options_tmp = options_tmp.drop(columns=["best_bid", "best_offer"])
+
+    # 9. (paper) filter mid_price < 1/8
+    print(
+        "DF size before filtering out mid_price < 1/8: ",
+        len(options_tmp),
+        "\n",
+    )
+
+    mid_price_less_one_eighth_mask = options_tmp["mid_price"] < 0.125
+
+    print(
+        "DF size of options with mid_price < 1/8: ",
+        len(options_tmp[mid_price_less_one_eighth_mask]),
+        "\n",
+    )
+
+    options_tmp = options_tmp[~mid_price_less_one_eighth_mask].copy()
+
+    print(
+        "DF size after filtering out mid_price < 1/8: ",
+        len(options_tmp),
+        "\n",
+    )
+
+    # 10. Calculate days to expiry
+    options_tmp["days_to_expiry"] = (
+        options_tmp["exdate"] - options_tmp["date"]
+    ).dt.days
+    options_tmp = options_tmp.astype({"days_to_expiry": "Int64"})
+
+    # 11. (paper) Filter days_to_expiry > 120
+    print(
+        "DF size before filtering out days_to_expiry > 120: ",
+        len(options_tmp),
+        "\n",
+    )
+
+    days_to_expiry_more_120_mask = options_tmp["days_to_expiry"] > 120
+
+    print(
+        "DF size of options with days_to_expiry > 120: ",
+        len(options_tmp[days_to_expiry_more_120_mask]),
+        "\n",
+    )
+
+    options_tmp = options_tmp.loc[~days_to_expiry_more_120_mask].copy()
+
+    print(
+        "DF size after filtering out days_to_expiry > 120: ",
+        len(options_tmp),
+        "\n",
+    )
+
+    options_tmp
+    return (options_tmp,)
+
+
+@app.cell(hide_code=True)
+def clean_forwards_md():
+    mo.md(r"""
+    ## Clean Forwards
+
+    Steps taken:
+    1. Keep necessary columns
+    2. Set the right dtypes
+    3. Filter for specific dates
+    """)
+    return
+
+
+@app.cell
+def clean_forwards(forwards):
+    # 1. Keep necessary columns
+    forwards_tmp = forwards.loc[
+        :,
+        ["date", "ForwardPrice", "expiration", "AMSettlement"],
+    ].copy()
+
+    print("Forward's columns: ", "\n", forwards_tmp.columns, "\n")
+
+    # 2. Set the right dtypes
+    forwards_tmp["date"] = pd.to_datetime(forwards_tmp["date"])
+    forwards_tmp["expiration"] = pd.to_datetime(forwards_tmp["expiration"])
+
+    forwards_tmp = forwards_tmp.astype(
+        {
+            "ForwardPrice": "Float64",
+        }
+    )
+
+    print("Option's datatype check: ", "\n", forwards_tmp.dtypes, "\n")
+
+    # 3. Filter for specific dates
+    forwards_date_mask = (forwards_tmp["date"] >= START_DATE) & (
+        forwards_tmp["date"] <= END_DATE
+    )
+    forwards_tmp = forwards_tmp[forwards_date_mask].copy()
+
+    print("Maximum date: ", forwards_tmp["date"].max(), "\n")
+    print("Minimum date: ", forwards_tmp["date"].min(), "\n")
+
+    forwards_tmp
+    return (forwards_tmp,)
+
+
+@app.cell(hide_code=True)
+def merge_md():
+    mo.md(r"""
+    # Merge
+
+    Filter steps:
+    1. Compute moneyness $\frac{F_t}{K}$
+    2. Compute log moneyness $\log \frac{F_t}{K}$
+    """)
+    return
+
+
+@app.cell
+def merge(forwards_tmp, options_tmp):
+    combined = options_tmp.merge(
+        forwards_tmp,
+        left_on=["date", "exdate", "am_settlement"],
+        right_on=["date", "expiration", "AMSettlement"],
+        how="left",
+    ).reset_index(drop=True)
+
+    combined = combined.drop(
+        columns=["expiration", "am_settlement", "AMSettlement"]
+    ).sort_values(by="date")
+
+    # 1. Compute moneyness
+    combined["moneyness"] = combined["ForwardPrice"] / combined["strike_price"]
+
+    # 2. Compute log moneyness
+    combined["log_moneyness"] = np.log(
+        combined["ForwardPrice"] / combined["strike_price"]
+    )
+
+    # Check for rows with nulls
+    print(
+        "Check for rows with null values:", "\n", combined[combined.isna().any(axis=1)]
+    )
+
+    combined
+    return (combined,)
+
+
+@app.cell(hide_code=True)
+def plot_volatility_smile_moneyness(combined):
+    sns.set_theme(style="whitegrid", context="talk")
+
+    plt.figure(figsize=(14, 8))  # Slightly larger for detail
+
+    sns.scatterplot(
+        data=combined,
+        x="moneyness",
+        y="impl_volatility",
+        hue="cp_flag",
+        alpha=0.4,
+        palette={
+            "C": "#2ecc71",
+            "P": "#e74c3c",
+        },
+        edgecolor="w",
+        linewidth=0.3,
+        s=40,
+    )
+
+    plt.axvline(
+        x=1, color="#34495e", linestyle="--", linewidth=1.5, label="At-The-Money"
+    )
+
+    sns.despine(left=True, bottom=True)
+
+    plt.title("Implied Volatility Smile", fontsize=20, weight="bold", pad=20)
+    plt.xlabel("Moneyness ($F/K$)", fontsize=14, labelpad=10)
+    plt.ylabel("Implied Volatility", fontsize=14, labelpad=10)
+
+    plt.legend(
+        title="Option Type",
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.1),
+        ncol=2,
+        frameon=False,
+    )
+
+    plt.tight_layout()
+    plt.show()
+
+
+@app.cell(hide_code=True)
+def plot_volatility_smile_log_moneyness(combined):
+    sns.set_theme(style="whitegrid", context="talk")
+
+    plt.figure(figsize=(14, 8))  # Slightly larger for detail
+
+    sns.scatterplot(
+        data=combined,
+        x="log_moneyness",
+        y="impl_volatility",
+        hue="cp_flag",
+        alpha=0.4,
+        palette={
+            "C": "#2ecc71",
+            "P": "#e74c3c",
+        },
+        edgecolor="w",
+        linewidth=0.3,
+        s=40,
+    )
+
+    plt.axvline(
+        x=0, color="#34495e", linestyle="--", linewidth=1.5, label="At-The-Money"
+    )
+
+    sns.despine(left=True, bottom=True)
+
+    plt.title("Implied Volatility Smile", fontsize=20, weight="bold", pad=20)
+    plt.xlabel("Log-Moneyness ($Ln(F/K)$)", fontsize=14, labelpad=10)
+    plt.ylabel("Implied Volatility", fontsize=14, labelpad=10)
+
+    plt.legend(
+        title="Option Type",
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.1),
+        ncol=2,
+        frameon=False,
+    )
+
+    plt.tight_layout()
+    plt.show()
+
+
+@app.cell(hide_code=True)
+def keep_atm_md():
+    mo.md(r"""
+    Filter steps:
+    1. (paper) filter out options with moneyness > 1.1 or < 0.9
+    """)
+
+
+@app.cell
+def keep_atm(combined):
+    # 1. (paper) filter out options with moneyness > 1.1 or < 0.9
+    print(
+        "DF size before filtering moneyness > 1.1 or < 0.9: ",
+        len(combined),
+        "\n",
+    )
+
+    moneyness_mask = (combined["moneyness"] > 1.1) | (combined["moneyness"] < 0.9)
+
+    print(
+        "DF size of options with moneyness > 1.1 or < 0.9: ",
+        len(combined[moneyness_mask]),
+        "\n",
+    )
+
+    combined1 = combined.loc[~moneyness_mask].copy()
+
+    print(
+        "DF size after filtering moneyness > 1.1 or < 0.9: ",
+        len(combined1),
+        "\n",
+    )
+
+    return combined1
+
+
+@app.cell(hide_code=True)
+def plot_volatility_smile_moneyness_filter(combined1):
+    sns.set_theme(style="whitegrid", context="talk")
+
+    plt.figure(figsize=(14, 8))  # Slightly larger for detail
+
+    sns.scatterplot(
+        data=combined1,
+        x="moneyness",
+        y="impl_volatility",
+        hue="cp_flag",
+        alpha=0.4,
+        palette={
+            "C": "#2ecc71",
+            "P": "#e74c3c",
+        },
+        edgecolor="w",
+        linewidth=0.3,
+        s=40,
+    )
+
+    plt.axvline(
+        x=1, color="#34495e", linestyle="--", linewidth=1.5, label="At-The-Money"
+    )
+
+    sns.despine(left=True, bottom=True)
+
+    plt.title("Implied Volatility Smile", fontsize=20, weight="bold", pad=20)
+    plt.xlabel("Moneyness ($F/K$)", fontsize=14, labelpad=10)
+    plt.ylabel("Implied Volatility", fontsize=14, labelpad=10)
+
+    plt.legend(
+        title="Option Type",
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.1),
+        ncol=2,
+        frameon=False,
+    )
+
+    plt.tight_layout()
+    plt.show()
+
+
+@app.cell(hide_code=True)
+def plot_volatility_smile_log_moneyness_filter(combined1):
+    sns.set_theme(style="whitegrid", context="talk")
+
+    plt.figure(figsize=(14, 8))  # Slightly larger for detail
+
+    sns.scatterplot(
+        data=combined1,
+        x="log_moneyness",
+        y="impl_volatility",
+        hue="cp_flag",
+        alpha=0.4,
+        palette={
+            "C": "#2ecc71",
+            "P": "#e74c3c",
+        },
+        edgecolor="w",
+        linewidth=0.3,
+        s=40,
+    )
+
+    plt.axvline(
+        x=0, color="#34495e", linestyle="--", linewidth=1.5, label="At-The-Money"
+    )
+
+    sns.despine(left=True, bottom=True)
+
+    plt.title("Implied Volatility Smile", fontsize=20, weight="bold", pad=20)
+    plt.xlabel("Log-Moneyness ($Ln(F/K)$)", fontsize=14, labelpad=10)
+    plt.ylabel("Implied Volatility", fontsize=14, labelpad=10)
+
+    plt.legend(
+        title="Option Type",
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.1),
+        ncol=2,
+        frameon=False,
+    )
+
+    plt.tight_layout()
+    plt.show()
+
+
+@app.cell(hide_code=True)
+def export_md():
+    mo.md(r"""
+    # Export
+    """)
+    return
+
+
+@app.cell
+def export(combined1):
+    combined1.sort_values(by="date").reset_index(drop=True).to_parquet(
+        "cleaned_data.parquet"
+    )
+    return
+
+
+if __name__ == "__main__":
+    app.run()
